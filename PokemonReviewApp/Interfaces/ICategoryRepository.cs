a1598775e48c6a7c07bb00bc252b7c42b99e8793
jonathanguyen21 <119833065+jonathanguyen21@users.noreply.github.com>
2024-02-22 11:13:19 -0800

    Add project files.

---
+++ PokemonReviewApp/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using PokemonReviewApp.Models;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+        bool CategoryExists(int id);
+
+    }
+}
